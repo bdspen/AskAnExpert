c1fd2d82feb4956dfe1e74e258766039a25ec245
Ben Spenard & Kevin Tokheim <student@epicodus.com>
2015-09-24 16:32:46 -0700

    add comment answer feature, feature currently not working

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -3,12 +3,16 @@ experts.controller('AnswersCtrl', function AnswersCtrl($scope, QuestionsFactory,
   $scope.QuestionsFactory = QuestionsFactory;
 
   $scope.addAnswer = function() {
-    $scope.question.answers.push({ name: $scope.expertAnswer, rating: 0 });
-    // $scope.question.answers.push("hello");
+    $scope.question.answers.push({ name: $scope.expertAnswer, rating: 0 , comments: [] });
     $scope.expertAnswer = null;
   };
 
   $scope.rate = function(answer, value) {
     answer.rating += value;
+  }
+  $scope.addComment = function(answer, comment) {
+    var newComment = {description: comment};
+    answer.comments.push(newComment);
+    $scope.comment = null;
   }
 });
